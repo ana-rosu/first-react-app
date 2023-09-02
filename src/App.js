871bac4c283b83217d5e858451caf6ecdde6e43e
Ana Roșu <anarosu766@gmail.com>
2023-09-02 17:26:22 +0300

    class components version complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import CardList from "./components/card-list/card-list.component";
+import SearchBox from "./components/search-box/search-box.component";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// component = self contained piece of code that returns some visual UI representation of HTML
+// jsx has made it easier to tell our App function what we want it to actually render visually
+//callback function in setState is going to run only after this state is fully updated
+// whenever i use .map i want to make sure that at the highest level of this element i add the unique key, so react knows what exactly should re render, it is much more performant and efficient when it comes to differentiating these components from each other and figure out which one should be updated when the value changes
+// the moment your component is placed onto the DOM is when you want to make an api request
+// constructor is first called always, it initialize the state, then render method it renders the initial ui, then componenentDidMount which fetches some data and calls setState which updates the state and makes react rerender the ui component
+// a component is meant to tie together functionality and visual representation of our ui
+// to tie together reusable portions of the code
+// whenever props change our component will also rerender
+// !!! Components rerender based on two conditions: when setState gets called and when props are updated !!!
+// no matter where  you import a css file it is going to be applicable to the entire website. we import it at component level for us, css in js libraries will help us with isolation
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [],
+      searchField: "",
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(
+          () => {
+            return { monsters: users };
+          }
+          // () => {
+          //   console.log(this.state);
+          // }
+        )
+      );
+  }
+
+  onSearchChange = (event) => {
+    const searchField = event.target.value.toLocaleLowerCase();
+
+    this.setState(() => {
+      return { searchField };
+    });
+  };
+
+  render() {
+    const { monsters, searchField } = this.state;
+    const { onSearchChange } = this;
+
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    });
+    return (
+      <div className="App">
+        <h1 className="app-title">Monster Rolodex</h1>
+        <SearchBox
+          onChangeHandler={onSearchChange}
+          placeholder="Search monsters..."
+          className="monsters-search-box"
+        />
+        <CardList monsters={filteredMonsters} />
+      </div>
+    );
+  }
 }
 
 export default App;
